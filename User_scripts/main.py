a9fad97a3e0324fea5b4c34fce24e5a1533918dc
OscarDLRL <oscardelarosalopez05@gmail.com>
2026-05-09 17:17:58 -0600

    pyinput Doesnt work lmaooooooo (for wayland)

--- User_scripts/main.py
+++ User_scripts/main.py
@@ -1,22 +1,56 @@
 import requests
-from pynput import keyboard
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
 
 Laptop = True
 
 urlRasp = "http://192.168.1.80:5000/data"
 urlLaptop = "http://192.168.1.137:5000/data"
 
-data = {
-    "forward": 1,
+current_state = {
+    "forward": 0,
     "backward": 0,
     "left": 0,
-    "right": 1,
-    "led": "red"
+    "right": 0
 }
 
-if Laptop:
-    response = requests.post(urlLaptop, json=data)
-else:
-    response = requests.post(urlRasp, json=data)
+#Sends any combination of w,a,s and d inputs
+def sendInput():
+    if Laptop:
+        response = requests.post(urlLaptop, json=current_state)
+    else:
+        response = requests.post(urlRasp, json=current_state) 
+    print(current_state)
 
-print(response.json())
+
+# Application builder
+class RoboFriendWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = RoboFriendWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
