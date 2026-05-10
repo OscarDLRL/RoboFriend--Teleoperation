0c5e7f4971a547534c58875fe1a422d58410e8c2
OscarDLRL <oscardelarosalopez05@gmail.com>
2026-05-09 20:37:05 -0600

    Added simple GUI that connects to URL, rasp code still needs work

--- User_scripts/main.py
+++ User_scripts/main.py
@@ -1,15 +1,17 @@
 import requests
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
+import cv2
 
 
 Laptop = True
 
-urlRasp = "http://192.168.1.80:5000/data"
-urlLaptop = "http://192.168.1.137:5000/data"
+urlRasp = "http://192.168.1.141:5000"
+urlLaptop = "http://192.168.1.137:5000"
+urlVideo = urlRasp + "/video_feed"
 
 current_state = {
+
     "forward": 0,
     "backward": 0,
     "left": 0,
@@ -19,9 +21,9 @@ current_state = {
 #Sends any combination of w,a,s and d inputs
 def sendInput():
     if Laptop:
-        response = requests.post(urlLaptop, json=current_state)
+        response = requests.post(urlLaptop + "/data", json=current_state)
     else:
-        response = requests.post(urlRasp, json=current_state) 
+        response = requests.post(urlRasp + "/data", json=current_state) 
     print(current_state)
 
 
@@ -30,27 +32,104 @@ class RoboFriendWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.setWindowTitle("RoboFriend Teleop")
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",alignment=QtCore.Qt.AlignCenter)
 
+        #Video GUI parameters
+        self.video_feed =QtWidgets.QLabel(
+            "Video Here",
+            alignment=QtCore.Qt.AlignCenter
+        )
+        self.video_feed.setFixedSize(640, 480)
+        self.cap = cv2.VideoCapture(urlVideo)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.video_updater)
+        self.timer.start(30)
+
+        self.text = QtWidgets.QLabel(
+            "Use wasd for movement",
+            alignment=QtCore.Qt.AlignCenter
+        )
+
+        # Layout of GUI
         self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.video_feed)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        # Enables Keyboard
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
 
-        self.button.clicked.connect(self.magic)
+    # VIDEO, updates image to the GUI taken fron URL
+    def video_updater(self):
+        ret,frame = self.cap.read()
+        if not ret:
+            self.video_feed.setText("No video frame")
+            return
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+        h, w, ch = frame.shape
+        bytes_per_line = ch * w
+
+        qt_image = QtGui.QImage(
+            frame.data,
+            w,
+            h,
+            bytes_per_line,
+            QtGui.QImage.Format_RGB888
+        )
+
+        pixmap = QtGui.QPixmap.fromImage(qt_image)
+        self.video_feed.setPixmap(pixmap)
+
+    # KEY PRESSED
+    def keyPressEvent(self, event):
+
+        if event.key() == QtCore.Qt.Key_W:
+            current_state["forward"] = 1
+
+        elif event.key() == QtCore.Qt.Key_S:
+            current_state["backward"] = 1
+
+        elif event.key() == QtCore.Qt.Key_A:
+            current_state["left"] = 1
+
+        elif event.key() == QtCore.Qt.Key_D:
+            current_state["right"] = 1
+
+        self.text.setText(str(current_state))
+
+        sendInput()
+
+
+    # KEY RELEASED
+    def keyReleaseEvent(self, event):
+
+        if event.key() == QtCore.Qt.Key_W:
+            current_state["forward"] = 0
+
+        elif event.key() == QtCore.Qt.Key_S:
+            current_state["backward"] = 0
+
+        elif event.key() == QtCore.Qt.Key_A:
+            current_state["left"] = 0
+
+        elif event.key() == QtCore.Qt.Key_D:
+            current_state["right"] = 0
+
+        self.text.setText(str(current_state))
+
+        sendInput()
+
 
 
 if __name__ == "__main__":
+    
     app = QtWidgets.QApplication([])
 
+    
+
     widget = RoboFriendWidget()
-    widget.resize(800, 600)
+    widget.resize(1000, 1000)
     widget.show()
 
     sys.exit(app.exec())
