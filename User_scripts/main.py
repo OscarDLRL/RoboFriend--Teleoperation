8de9c11d7fd86ec779d76ddbbbd66920c6890bf1
OscarDLRL <oscardelarosalopez05@gmail.com>
2026-05-10 13:09:53 -0600

    Threading works, probably will delete because i realized the existence of networkManager

--- User_scripts/main.py
+++ User_scripts/main.py
@@ -3,7 +3,9 @@
 import requests
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Qt, Slot, QThread, Signal, QObject, QRunnable, QThreadPool
 import cv2
+import time
 
 
 Laptop = False
@@ -20,14 +22,6 @@ current_state = {
     "right": 0
 }
 
-#Sends any combination of w,a,s and d inputs
-def sendInput():
-    if Laptop:
-        response = requests.post(urlLaptop + "/data", json=current_state)
-    else:
-        response = requests.post(urlRasp + "/data", json=current_state) 
-    print(current_state)
-
 
 # Application builder
 class RoboFriendWidget(QtWidgets.QWidget):
@@ -37,21 +31,28 @@ class RoboFriendWidget(QtWidgets.QWidget):
         self.setWindowTitle("RoboFriend Teleop")
 
 
-        #Video GUI parameters (updates every 30ms)
+        #Video GUI parameters 
         self.video_feed =QtWidgets.QLabel(
             "Video Here",
-            alignment=QtCore.Qt.AlignCenter
+            alignment=Qt.AlignCenter
         )
         self.video_feed.setFixedSize(640, 480)
-        self.cap = cv2.VideoCapture(urlVideo)
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.video_updater)
-        self.timer.start(30)
+
+        # Video Thread Initialitation
+        self.video_thread = QThread()
+        self.video_worker = VideoWorker(urlVideo)
+        self.video_worker.moveToThread(self.video_thread) #Moves video_worker to the new Thread
+        self.video_thread.started.connect(self.video_worker.run)
+        self.video_worker.frame_ready.connect(self.video_updater) #Conects Video to the GUI
+        self.video_thread.start()
+
+        # Global ThreadPool (can share with other tasks, current;y only wasd) 
+        self.threadpool = QThreadPool.globalInstance()
 
         # Instruction text
         self.text = QtWidgets.QLabel(
             "Use wasd for movement",
-            alignment=QtCore.Qt.AlignCenter
+            alignment=Qt.AlignCenter
         )
 
         # Layout of GUI
@@ -61,13 +62,10 @@ class RoboFriendWidget(QtWidgets.QWidget):
         # Enables Keyboard
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
 
-    # VIDEO, updates image to the GUI taken fron URL
-    def video_updater(self):
-        ret,frame = self.cap.read()
-        if not ret:
-            self.video_feed.setText("No video frame")
-            return
-        
+
+    # VIDEO (Receives the frame emitted by the worker)
+    @Slot(object)
+    def video_updater(self,frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         h, w, ch = frame.shape
@@ -83,9 +81,20 @@ class RoboFriendWidget(QtWidgets.QWidget):
 
         pixmap = QtGui.QPixmap.fromImage(qt_image)
         self.video_feed.setPixmap(pixmap)
+    
+    # Conects to sendInputs
+    def send_input_async(self):
+        url = urlLaptop if Laptop else urlRasp
+
+        task = SendInputTask(url, current_state)
+
+        self.threadpool.start(task)
 
     # KEY PRESSED
     def keyPressEvent(self, event):
+        # Checks if the key is being prssed down and doesnt send the same input multiple times
+        if event.isAutoRepeat():
+            return
 
         if event.key() == QtCore.Qt.Key_W:
             current_state["forward"] = 1
@@ -100,12 +109,13 @@ class RoboFriendWidget(QtWidgets.QWidget):
             current_state["right"] = 1
 
         self.text.setText(str(current_state))
-
-        sendInput()
-
+        self.send_input_async()
 
     # KEY RELEASED
     def keyReleaseEvent(self, event):
+        # Checks if the key is being prssed down and doesnt send the same input multiple times
+        if event.isAutoRepeat():
+            return
 
         if event.key() == QtCore.Qt.Key_W:
             current_state["forward"] = 0
@@ -120,8 +130,48 @@ class RoboFriendWidget(QtWidgets.QWidget):
             current_state["right"] = 0
 
         self.text.setText(str(current_state))
+        self.send_input_async()
 
-        sendInput()
+# Different thread for video, QThread is perfect for long tasks, it will crash without this
+class VideoWorker(QObject):
+
+    frame_ready = Signal(object)
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.running = True
+
+    @Slot()
+    def run(self):
+        self.cap = cv2.VideoCapture(self.url)
+
+        while self.running:
+            ret, frame = self.cap.read()
+
+            if not ret:
+                continue
+
+            self.frame_ready.emit(frame)
+
+            # FPS control, stops every 30 ms
+            time.sleep(0.03)
+
+        self.cap.release()
+
+    @Slot()
+    def stop(self):
+        self.running = False
+
+
+class SendInputTask(QRunnable):
+    def __init__(self, url, state):
+        super().__init__()
+        self.url = url
+        self.state = state.copy()
+
+    #Sends any combination of w,a,s and d inputs
+    def run(self):
+        response = requests.post(self.url + "/data", json=self.state) 
 
 
 
@@ -129,7 +179,7 @@ if __name__ == "__main__":
     
     app = QtWidgets.QApplication([])
     widget = RoboFriendWidget()
-    widget.resize(1000, 1000)
+    widget.resize(680, 1000)
     widget.show()
 
     sys.exit(app.exec())
